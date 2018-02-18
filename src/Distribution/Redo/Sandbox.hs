3f0b247946cf71f5fb79f61a260a8069ef08c2f6
Okuno Zankoku <zankoku.okuno@gmail.com>
2018-02-18 13:07:01 -0500

    first test of finding a project; first draft of implementation

--- src/Distribution/Redo/Sandbox.hs
+++ src/Distribution/Redo/Sandbox.hs
@@ -2,3 +2,23 @@
     Find projects; parse & store project configuration.
 -}
 module Distribution.Redo.Sandbox where
+
+import Control.Monad
+
+import System.FilePath
+import System.Directory
+
+
+data Sandbox = Sandbox
+    { _redoDir :: FilePath
+    }
+
+getSandbox :: FilePath -> IO Sandbox
+getSandbox filepath = do
+    startDir <- makeAbsolute $ dropTrailingPathSeparator filepath
+    let candidates = (</> ".redo") <$> iterateUp startDir
+    redoDirs <- filterM doesDirectoryExist candidates
+    let _redoDir = head redoDirs -- FIXME
+    pure $ Sandbox {..}
+    where
+    iterateUp dir0 = let dir' = takeDirectory dir0 in if dir' == dir0 then [dir0] else dir0 : iterateUp dir'
--- test/ProjectFinding.hs
+++ test/ProjectFinding.hs
@@ -1,8 +1,28 @@
 import TestUtil
 
+import Distribution.Redo.Sandbox
+
+import System.Directory
+import System.FilePath
+
 main = runTests "Project Finding"
     [ Test
-        { name = "find `.redo` in current directory"
-        , run = pure $ Fail "unimplemented"
+        { name = "find `.redo` in same directory"
+        , run = do
+            cwd <- getCurrentDirectory
+            let projectDirRel = "test/project-finding/project-dir"
+                projectDirAbs = cwd </> projectDirRel
+                projectDirSlash = projectDirAbs ++ "/"
+                expectedRedoDir = projectDirAbs </> ".redo"
+            abs_result <- _redoDir <$> getSandbox projectDirAbs
+            rel_result <- _redoDir <$> getSandbox projectDirRel
+            slash_result <- _redoDir <$> getSandbox projectDirSlash
+            pure $ if
+                    | abs_result `notEqualFilePath` expectedRedoDir -> Fail $ concat [abs_result, " != ", expectedRedoDir]
+                    | rel_result `notEqualFilePath` expectedRedoDir -> Fail $ concat [rel_result, " != ", expectedRedoDir]
+                    | slash_result `notEqualFilePath` expectedRedoDir -> Fail $ concat [slash_result, " != ", expectedRedoDir]
+                    | otherwise -> Pass
         }
     ]
+
+notEqualFilePath a b = not $ equalFilePath a b
